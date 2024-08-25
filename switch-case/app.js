3a80f687c43eed269c8905eb5046ee50e276540e
abdulkhamidov <aabdulkhamidov818@gmail.com>
2024-08-25 13:41:36 +0500

    switch case problems

---
+++ switch-case/app.js
@@ -0,0 +1,110 @@
+// Case1. K butun soni berilgan. Baho natijalarini
+//chiqaruvchi programma tuzing.(1 - yomon, 2 - qoniqarsiz,
+//3 - qoniqarli, 4 - yahshi, 5 - a'lo). Agar k soni 1-5 gacha
+//oraliqqa tegishli bo`lmasa "xato" deb chiqarilsin.
+
+// let k = 5;
+
+// switch (k) {
+//   case 1:
+//     console.log("yomon");
+//     break;
+//   case 2:
+//     console.log("qoniqarsz");
+//     break;
+//   case 3:
+//     console.log("qoniqarli");
+//     break;
+//   case 4:
+//     console.log("yaxshi");
+//     break;
+//   case 5:
+//     console.log("a'lo");
+//     break;
+//   default:
+//     console.log("no data");
+// }
+
+//===============================================
+
+// Case2. Oy raqamini berilgan.
+//  Kiritilgan oy qaysi faslga tegishli ekanligini chiqaruvchi programma tuzilsin.
+//  (Masalan: 2 chi oy, "qish")
+
+// let number = 10;
+
+// switch (number) {
+//   case 12:
+//   case 1:
+//   case 2:
+//     console.log("qish");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("bahor");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("yoz");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("kuz");
+//     break;
+//   default:
+//     console.log("no data");
+// }
+
+//============================================
+
+// Case3. Oy raqami berilgan. Shu oyda nechta kun borligini aniqlovchi programma tuzilsin.
+
+// let number = 1;
+
+// switch (number) {
+//   case 1:
+//   case 3:
+//   case 5:
+//   case 7:
+//   case 8:
+//   case 10:
+//   case 12:
+//     console.log(" shu oyda 31 kun bor");
+//     break;
+//   case 4:
+//   case 6:
+//   case 9:
+//   case 11:
+//     console.log("shu oyda 30kun bor");
+//     break;
+//   case 2:
+//     console.log("shu oyda 28 kub faqat kabisa yilida 29 kun boladi");
+//     break;
+//   default:
+//     console.log("no data");
+// }
+
+//===================================
+
+// Case4. Uzinlik birliklari quyidagi tartibda berilgan.
+// 1 - desimetr, 2 - kilometr, 3 - metr, 4 - millimeter,
+//5 - santimetr.Uzunlik birligini bildiruvchi son
+//berilgan(1 - 5 oraliqda) va shu birlikdagi kesma uzunligi
+//berilgan(haqiqiy son).Kesmaning uzunligini metrlarda ifodalofchi programma tuzilsin.
+
+// Case5. Og'irlik birliklari quyidagi tartibda berilgan. 1-kilogramm, 2-milligramm, 3-gramm, 4-tonna, 5- sentner. Og'irlik birligini bildiruvchi soni berilgan va shu birlikdagi og`irlik qiymati berilgan. Og`irlikni kilogramda ifodalofchi programma tuzilsin.
+
+// Case6. Sanani bildiruvchi ikkita butun son berilgan D (kun) va M (oy). (Kabisa bo`lmagan yil sanasi kiritiladi). Berilgan sanani ifodalovchi programma tuzilsin. Kabisa yilida 366 kun, kabisa bo'lmagan yilda 365 kun bor bo'ladi.
+
+// Case7. Ikkita butun son berilgan D (kun) va M (oy). (Kabisa bo`lmagan yil sanasi kiritiladi). Berilgan sanadan keyingi sanani ifodalovchi programma tuzilsin.
+
+// Case8. Robot faqat to`rtta tomonga ko`cha oladi ("s"-shimol, "j"-janub, "q"-sharq, "g"-g'arb) va uchta raqamli kamanda: 0-harakni davom ettir, 1-chapga buril, 2-o`ngga buril. Y - robot yo`nalishi va K - kamanda berilgan. Berilgan kamanda bajarilgandan keying robot holatini aniqlovchi programma tuzilsin.
+
+// Case9. O'yin kartasi turlari berilgan 1-g`isht, 2-olma, 3-chillak, 4-qarg`a. 10 lik kartadan katta kartalar quyidagi qiymatlarni o`zlashtirgan: 11-valet, 12-dama, 13-qirol, 14-tuz. Ikkita butun son berilgan N-karta qiymati (6≤ N ≤14), M-karta turi(1≤M ≤4) kiritilganda karta nomlarini (masalan: "olti qarg`a”) chiqarib beruvchi programma tuzilsin.
+
+// Case10. O`quv masalalarini aniqlovchi 10-40 gacha butun son berilgan. Son kiritilganda unga mos so`zlarda ifodalovchi programma tuzilsin. ("yigirmata masala", "o`n uchta masala" va h.k.)
+
+// Case11. 100-999 gacha oraliqdagi sonlarni so'zlarda ifodalovchi programma tuzilsin. (masalan: 123- "bir yuz yigirma uch").
