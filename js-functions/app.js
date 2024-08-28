6e3f10878d2feeaf13cadc71753e1787f7361f2b
abdulkhamidov <aabdulkhamidov818@gmail.com>
2024-08-28 20:55:15 +0500

    js function problems

---
+++ js-functions/app.js
@@ -0,0 +1,122 @@
+// 1.Funksiya yasang, ushbu funksiya 2ta argument qabul qilsin
+// va ularning yig’indisini qaytarib bersin.
+
+// function calcFunc(x, y) {
+//   return x + y;
+// }
+
+// let result = calcFunc(3, 4);
+// console.log(result);
+
+// 2.Minutlarni butun son sifatida qabul qilib, uni sekundlarga
+// o’girib beradigan funksiya yasang.
+
+// function toSecond(min) {
+//   return min * 60;
+// }
+
+// let result = toSecond(2);
+
+// console.log(result);
+
+//==================================
+// 3.Funksiya butun son qabul qiladi. Funksiya ushbu butun
+// sondan keyingi sonni qaytarsin.
+
+// function nextInteger(num) {
+//   return num + 1;
+// }
+// let result = nextInteger(9);
+
+// console.log(result);
+
+//========================================
+
+// 4.Funskiya uchburchakning asosi va balandligini qabul qiladi.
+// Funksiya uchburchakning yuzini hisoblab qaytarsin.
+// Uchburchak yuzini topish formulasi: S = (asos * balandlik) / 2
+
+// function uchburchakYuzi(asos, balandlik) {
+//   let s = (asos * balandlik) / 2;
+//   return s;
+// }
+
+// let result = uchburchakYuzi(4, 9);
+// console.log(result);
+
+//===================================
+// 5.Funksiya yoshni yil ko’rinishida qabul qiladi. ushbu funksiya
+// yoshni kunlarda hisoblab natijani qaytarsin. Bir yil 365 kun deb
+// oling.
+
+// function ageToDays(year) {
+//   return year * 365;
+// }
+
+// let result = ageToDays(24);
+
+// console.log(result);
+
+//==================================================
+
+// 6.Quyidagi namunalarga muvofiq ravishda funksiyani tana
+// qismini yozing.
+
+// kub(3) ᔍ 27
+// kub(5) ᔍ 125
+// // kub(10) ᔍ 1000
+
+// function toKub(num) {
+//   return num ** 3;
+// }
+// let result = toKub(5);
+// console.log(result);
+
+// //==============================
+// 7.Massivni ichidan birinchi elementini qaytaradigan funksiya
+// yasang.
+
+// function firstElement(arr) {
+//   return arr[0];
+// }
+
+// let result = firstElement([1, 2, 2, 3]);
+
+// console.log(result);
+//=======================================
+// 8.Funksiya 2ta argument qabul qilsin. Ushbu funksiya birinchi
+// qiymatning ikkinchi qiymat bilan ko’paytmasini qaytarsin.
+
+// function calcNumber(x, y) {
+//   return x * y;
+// }
+
+// let result = calcNumber(2, 2);
+
+// console.log(result);
+
+//===========================================
+// 9.Soat ko’rinishida qiymat kiritilsa, sekundlarda qaytaradigan
+// funksiya yasang.
+
+// function hourToSecond(hour) {
+//   return hour * 60 * 60;
+// }
+
+// let result = hourToSecond(2);
+
+// console.log(result);
+
+//=======================================
+
+// 10.Uchburchakning uchinchi tomonining eng uzun qiymatini
+// qaytaradigan funksiya yasang.
+// Formula: Eng uzun uchinchi tomon = (tomon1 + tomon2) - 1
+
+// function uchinchiTomon(tomon1, tomon2) {
+//   let tomon3 = tomon1 + tomon2 - 1;
+//   return tomon3;
+// }
+
+// let result = uchinchiTomon(9, 10);
+// console.log(result);
